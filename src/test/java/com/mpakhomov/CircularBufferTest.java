26896dbd4f220e180bc19f842d8e69bab7a65160
mpakhomov <mike.pakhomov@gmail.com>
2014-06-25 11:07:31 +0400

    updated

---
+++ src/test/java/com/mpakhomov/CircularBufferTest.java
@@ -0,0 +1,22 @@
+package com.mpakhomov;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+
+import com.sun.corba.se.impl.orb.ParserTable;
+import org.junit.Test;
+
+public class CircularBufferTest {
+
+    @Test
+    public void testThatTheMostRecetnlyAddedElementIsDeleted() {
+        CircularBuffer<Integer> buf = new CircularBuffer<Integer>(3);
+        buf.offer(1);
+        buf.offer(2);
+        buf.offer(3);
+        buf.offer(4);
+        assertArrayEquals(buf.toArray(), new Object[] {2, 3, 4});
+    }
+}
---
+++ src/test/java/com/mpakhomov/SinglyLinkedListTest.java
@@ -0,0 +1,7 @@
+package com.mpakhomov;
+
+/**
+ * Created by mike on 6/24/14.
+ */
+public class SinglyLinkedListTest {
+}
