8616b850cc3a88f74f7b1b28c5a467a1048cbe73
Mike Pakhomov <mike.pakhomov@gmail.com>
2016-11-13 19:40:49 -0500

    slightly modified InsertionSort

--- src/main/java/com/mpakhomov/algorithms/sort/InsertionSort.java
+++ src/main/java/com/mpakhomov/algorithms/sort/InsertionSort.java
@@ -38,8 +38,8 @@ public class InsertionSort {
      */
     static public void sort(int[] a) {
         for (int i = 1; i < a.length; i++) {
-            for (int j = i - 1; j >= 0 && a[j] > a[j + 1]; j--) {
-                exch(a, j, j + 1);
+            for (int j = i; j > 0 && a[j] < a[j - 1]; j--) {
+                exch(a, j, j - 1);
             }
         }
     }
